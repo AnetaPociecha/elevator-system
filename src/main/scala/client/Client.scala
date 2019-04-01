8b191d8bcad50784a38a48efb714fedf56e1cc67
anetapociecha <apociecha@interia.pl>
2019-04-02 01:27:10 +0200

    client

--- src/main/scala/client/Client.scala
+++ src/main/scala/client/Client.scala
@@ -1,5 +1,31 @@
 package client
 
-class Client {
+import elevatorsystem.{Elevator, ElevatorSystem}
 
+object Client {
+
+  val elevators: List[Elevator] = List(
+    new Elevator(1,2,2),
+    new Elevator(2,0,0)
+  )
+  val elevatorSystem: ElevatorSystem
+    = new ElevatorSystem(elevators)
+
+  def main(args: Array[String]): Unit = {
+
+    var status = elevatorSystem.status()
+    println(status)
+
+    elevatorSystem.pickup(4,1)
+    elevatorSystem.step()
+    elevatorSystem.step()
+
+    status = elevatorSystem.status()
+    println(status)
+
+    elevatorSystem.update(2,4,6)
+
+    status = elevatorSystem.status()
+    println(status)
+  }
 }
